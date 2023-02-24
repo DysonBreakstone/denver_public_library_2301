c4cf2e6901620001fed771f41a844425b56f9515
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-24 09:22:23 -0700

    add tests for Book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,19 @@
+require './lib/book.rb'
+
+RSpec.describe Book do
+
+  before(:each) do
+    book_1 = Book.new({author_first_name: "John", author_last_name: "Crowley", title: "Engine Summer", publication_date: "March 12 1979"})
+  end
+    
+  it "exists" do
+    expect(book_1).to be_a(Book)
+  end
+
+  it "has attributes" do 
+    expect(book_1.title).to eq("Engine Summer")
+    expect(book_1.author).to eq("John Crowley")
+    expect(book_1.publication_year).to eq("1979")
+  end
+
+end
